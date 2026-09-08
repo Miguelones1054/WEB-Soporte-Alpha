4038773ef4af962ac97e68fed7c9f37f1c0c8ea0
Miguelones1054 <2miguelones@gmail.com>
2026-09-07 21:37:39 -0500

    1212

--- src/app/admin-panel/creditos/page.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import { HubSectionRedirect } from '../views/HubSectionRedirect';
-
-export default function CreditosPage() {
-  return <HubSectionRedirect section="creditos" />;
-}
